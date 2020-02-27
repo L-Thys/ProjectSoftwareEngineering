9e2bf44af45862457e43b3fbc96d6e4626e596cf
L-Thys <Lthys54@gmail.com>
2020-02-27 15:17:39 +0100

    add tinyXML

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 //
 // Created by lotte on 27.02.20.
 //
+#include "tinyXML/tinyxml.h"
+#include "iostream"
+
 int main(){
+    TiXmlDocument doc;
+    if(!doc.LoadFile("../test.xml")) {
+        std::cerr << doc.ErrorDesc() << std::endl;
+        return 1;
+    }
     return 0;
 }
