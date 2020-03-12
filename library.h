82f797a170a686df932203acebf8c1db1db0aa50
Bas-Tobback <bas.tobback@student.uantwerpen.be>
2020-03-12 20:27:18 +0100

    Extra documentation, not every class is already handled. The Spoor class is momentarily not used anymore for the purpose of simplification. Also is the getSpoor function somewhat different because we now have a vector of Spoor objects and we have only 1 Spoor. This means that we now return simply the first (and only) integer in the vector. Also is the map of Spoor object temporarily deleted.

--- library.h
+++ library.h
@@ -5,13 +5,12 @@
 #ifndef PSE_LIBRARY_H
 #define PSE_LIBRARY_H
 
-// alle algemene libraries die nodig zijn in het project
+// all libraries that are probably going to be used in every file in this project
 #include <string>
 #include <vector>
 #include <map>
 #include <iostream>
 #include "OurExceptions.h"
 #include "gtest/include/gtest/gtest.h"
-
 
 #endif //PSE_LIBRARY_H
