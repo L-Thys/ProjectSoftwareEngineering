a8456dd3ab4d37d5e57bfb4813992b1dca1e66e0
Bas-Tobback <bas.tobback@student.uantwerpen.be>
2020-03-13 18:16:22 +0100

    Extra documentation, not every class is already handled. The Spoor class is momentarily not used anymore for the purpose of simplification. Also is the getSpoor function somewhat different because we now have a vector of Spoor objects and we have only 1 Spoor. This means that we now return simply the first (and only) integer in the vector. Also is the map of Spoor object temporarily deleted.

--- Metronet.cpp
+++ Metronet.cpp
@@ -22,39 +22,63 @@ void Metronet::addTram(Tram *tram) {
 
 bool Metronet::isConsistent() {
 
-    // we check every station in our network
+    // -- We check every station in our network -- //
     for (std::map<std::string, Station *>::iterator station = _stations.begin();
         station != _stations.end(); station++) {
 
-        // we checken of de standaard waarde van een spoor is aangepast of niet
+        // ~ Spoor is automatically set on -1 to easily check if Spoor is changed or not ~ //
         if (station->second->getSpoor() == -1) {
             return false;
         }
 
-        // we checken of de vorige of volgende van een station hetzelfde spoor heeft
-        if(Metronet::findStation(station->second->getVolgende())==NULL) return false; //check of volgende een station is dat bestaat
+        // ~ If the station has a valid Spoor, we check if the following and the previous Station also have this Spoor ~ //        if(Metronet::findStation(station->second->getVolgende())==NULL) return false; //check of volgende een station is dat bestaat
         else if (station->second->getSpoor() != Metronet::findStation(station->second->getVolgende())->getSpoor()){
-            return false;
+            return false;       // we return false if the equality of the integers in the Spoor member is false
         }
         if(Metronet::findStation(station->second->getVorige())==NULL) return false; //check of vorige een station is dat bestaat
         else if (station->second->getSpoor() != Metronet::findStation(station->second->getVorige())->getSpoor()){
-            return false;
+            return false;       // idem
         }
     }
+    // ------------------------------------------- //
 
-    for (std::map<int, Tram *>::iterator tram = _trams.begin(); tram != _trams.end(); tram++){
-        // we zoeken het homestation in het network
+    // -- We now check every tram inside _trams if they are initiated correctly -- //
+    std::vector<int> as_tracks;             // to be used later on
+
+      // tram is a pair <int Tram*>, we need to take tram.second to get the tram itself
+    for (std::map<int, Tram *>::iterator tram = _trams.begin(); tram != _trams.end(); tram++) {
+
+        // ~ Home is the start Station of our tram, we check if this station is a valid station in our network ~ //
         std::string home = tram->second->getStartStation();
-        if (Metronet::_stations.find(home) == Metronet::_stations.end()){
+        if (Metronet::_stations.find(home) == Metronet::_stations.end()) {
             return false;
         }
         if(Metronet::findStation(tram->second->getStartStation())==NULL) return false; //check of startstation een station is dat bestaat
         if (tram->second->getLijn() != Metronet::findStation(tram->second->getStartStation())->getSpoor()){
+
+        // ~ We also check if the Spoor of ou tram is equal to the Spoor that runs through its Start Station ~ //
+        if (tram->second->getLijn() != Metronet::findStation(tram->second->getStartStation())->getSpoor()) {
             return false;
         }
-    }
 
-// TODO : • elk spoor heeft exact 1 tram. ~EN~  • elk spoor komt maximaal 1 keer voor in een station.
+        // ~ This part checks if each Spoor occurs exactly 1 time ~ //
+            // we use the already initialised vector of int 'as_tracks' which stands for "already seen tracks"
+        for (std::vector<int>::iterator it = as_tracks.begin(); it != as_tracks.end(); it++){
+
+            // if the line is already is the as_tracks, it has been used so we can't use it again, we return false
+            if (*it == tram->second->getLijn()){
+                return false;
+            }
+            else {      // if the line is not present in the as_tracks
+                        // we push it in so we ensure that duplicates are noticed
+                as_tracks.push_back(tram->second->getLijn());
+            }
+        }
+
+    }
+    // --------------------------------------------------------------------------- //
+
+// TODO :• elk spoor komt maximaal 1 keer voor in een station.
 
     return true;
 }
