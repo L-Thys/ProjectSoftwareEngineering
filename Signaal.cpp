6e8592e54b36d1ce41c4c590807dd3daa3111c2f
Bas-Tobback <bas.tobback@student.uantwerpen.be>
2020-04-29 22:09:03 +0200

    contracten in Signaal.cpp

--- Signaal.cpp
+++ Signaal.cpp
@@ -15,13 +15,18 @@ Signaal::Signaal(int spoor, int limiet, const std::string &type, Station *vorige
 
 Signaal::Signaal(int spoor, const std::string &type, Station *vorige, Station *volgende) : spoor(spoor), type(type),
                                                                                            vorige(vorige),
-                                                                                           volgende(volgende) {}
+                                                                                           volgende(volgende) {
+    ENSURE(properlyInitialized(),"A constructor must end in a properlyInitialized state" );
+}
 
 int Signaal::getLimiet() const {
+    REQUIRE(properlyInitialized(), "The Signal was not or not properly initialized befor calling getLimiet");
+    REQUIRE(type == "SNELHEID", "The type of the signal must be \"SNELHEID\" when calling getLimiet");
     return limiet;
 }
 
 const std::string &Signaal::getType() const {
+    REQUIRE(properlyInitialized(), "The Signal was not or not properly initialized befor calling getType");
     return type;
 }
 
